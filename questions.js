9704b2e9146539f7d533809b9788b8f2df6f761a
cindoushka <cindie-baj@live.fr>
2022-12-02 07:47:36 +0100

    finale push

--- questions.js
+++ questions.js
@@ -1,13 +1,13 @@
 let tableauQuestions = [
     {   "id":0,
-        "img":"images/qriositeTypo.png",
+        "img":"images/bisou.jpg",
         "question":"Un camarade que tu ne souhaites pas embrasser insiste fortement pour que tu le fasses, que fais-tu?",
         "reponse":[
             {1: "Je finis par accepter 🤷‍♂️", nextQ: 1},
             {2: "Je refuse 🙅‍♀️", nextQ: 2}]
     },
     {   "id":1,
-        "img":"https://france3-regions.francetvinfo.fr/image/MhEIY3_fb5yH6GsCsu0uNPwTDdw/386x290/regions/2021/11/09/618aa02a7624d_000-8yy6pq-1-5510691.jpg",
+        "img":"images/no.jpg",
         "question":"Sache que le refus est OK, si tu n'as pas envie de quelque chose, refuse! Maintenant tu es en couple, vous souhaitez aller plus loin, quel est ton premier reflexe?",
         "reponse":[
             {1: "Vérifier qu'il a un préservatif 🛡", nextQ: 4},
@@ -15,7 +15,7 @@ let tableauQuestions = [
     },
     {
         "id":2,
-        "img":"images/qriosite.svg",
+        "img":"images/no.jpg",
         "question":"Très bien! Le refus est OK, ne jamais se forcer à faire ce qu'on ne veut pas! Maintenant tu es en couple, vous souhaitez aller plus loin, quel est ton premier reflexe?",
         "reponse":[
             {1: "Vérifier qu'il a un préservatif 🛡", nextQ: 4},
@@ -23,7 +23,7 @@ let tableauQuestions = [
     },
     {
         "id":3,
-        "img":"images/qriosite.svg",
+        "img":"images/preservatif.jpg",
         "question":"C'est normal de vouloir profiter de l'instant, en revanche n'oublie pas de te protéger c'est très important pour se protéger des IST et MST ;) Comme vous n'avez pas mis de préservatif, à quoi penses-tu le lendemain?",
         "reponse":[
             {1: "Je dois faire un depistage au plus vite! 💉", nextQ: 5},
@@ -31,7 +31,7 @@ let tableauQuestions = [
     },
     {
         "id":4,
-        "img":"images/qriosite.svg",
+        "img":"images/preservatif.jpg",
         "question":"Très bon reflexe! Malheureusement, le préservatif a craqué... Quel est ton premier reflexe?",
         "reponse":[
             {1: "Je dois faire un depistage au plus vite! 💉", nextQ: 5},
@@ -39,19 +39,49 @@ let tableauQuestions = [
     },
     {
         "id":5,
-        "img":"images/qriosite.svg",
+        "img":"images/piqures.jpg",
         "question":"Très bon reflexe! Sais-tu également qu'il existe un traitement préventif au VIH après un rapport à risque (le TPE)? Penses-vite à le demander! Ça fait maintenant longtemps que vous êtes ensemble et souhaitez ne plus mettre de préservatifs, comment procèdez-vous?",
         "reponse":[
-            {1: "Un nouveau depistage, et ce régulièrement", nextQ: 7},
-            {2: "On se fait confiance, on peut y aller!", nextQ: 8}]
+            {1: "Un nouveau depistage, et ce régulièrement 💉", nextQ: 7},
+            {2: "On se fait confiance, on peut y aller! ♥️", nextQ: 8}]
     },
     {
-        "id":5,
-        "img":"images/qriosite.svg",
-        "question":"Très bon reflexe! Sais-tu également qu'il existe un traitement préventif au VIH après un rapport à risque (le TPE)? Penses-vite à le demander! ",
+        "id":6,
+        "img":"images/piqure.jpg",
+        "question":"On te comprend mais il serait plus sage de penser au dépistage au plus vite pour vérifier qu'il n'y a pas de MST/IST et s'il y en a se soigner au plus vite! D'ailleurs maintenant ça fait longtemps que vous êtes ensemble et souhaitez ne plus mettre de préservatifs, comment procèdez-vous?",
         "reponse":[
-            {1: "Je dois faire un depistage au plus vite! 💉", nextQ: 4},
-            {2: "À recommencer 😋", nextQ: 3}]
+            {1: "Un nouveau depistage, et ce régulièrement 💉", nextQ: 7},
+            {2: "On se fait confiance, on peut y aller! ♥️", nextQ: 8}]
+    },
+    {
+        "id":7,
+        "img":"images/omg.jpg",
+        "question":"Super, tu es au point là dessus! Malheureusement, tu apprends que tu as la chlamydia...",
+        "reponse":[
+            {1: "Oh mon dieu je vais mourir! 😱", nextQ: 9},
+            {2: "Il y a un traitement non? 🤔", nextQ: 10}]
+    },
+    {
+        "id":8,
+        "img":"images/omg.jpg",
+        "question":"Et non, même avec une personne à qui on a confiance, il faut se tester avant ET après! D'ailleurs après un test, tu apprends que tu as la chlamydia malheureusement...",
+        "reponse":[
+            {1: "Oh mon dieu je vais mourir! 😱", nextQ: 9},
+            {2: "Il y a un traitement non? 🤔", nextQ: 10}]
+    },
+    {
+        "id":9,
+        "img":"images/ouf.jpg",
+        "question":"Non ne t'en fais pas! La chlamydia se soigne très bien! Prend ton traitement et c'est du passé, tu feras attention la prochaine fois!",
+        "reponse":[
+            {1: "Merci pour les infos!", nextQ: 0}]
+    },
+    {
+        "id":10,
+        "img":"images/ouf.jpg",
+        "question":"En effet la chlamydia se soigne très bien! Prend ton traitement et c'est du passé, tu feras attention la prochaine fois!",
+        "reponse":[
+            {1: "Merci pour les infos!", nextQ: 0}]
     },
 ]
 
